d907078a43434b62a5bd36f245d5f315607c627b
TechbyWebCoder <techbywedcoder@gmail.com>
2025-08-02 18:04:44 +0530

    Update and rename Star Pattern/02_hollow_square to Star Pattern/02_hollow_square /02_hollow_square.py

---
+++ Star Pattern/02_hollow_square /02_hollow_square.py
@@ -0,0 +1,16 @@
+#pattern 2
+#Hollow Square Pattern
+
+"""
+* * * * *
+*       *
+*       *
+*       *
+* * * * *
+"""
+
+for i in range(5):
+    if i ==0 or i == 4:
+        print("*" * 5)
+    else:
+        print("*   *")    
