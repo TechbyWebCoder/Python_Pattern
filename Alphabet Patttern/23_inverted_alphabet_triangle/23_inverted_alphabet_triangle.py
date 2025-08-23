eb01f5f2ae7d75a913c8064ff651bd0ebac0a190
TechbyWebCoder <techbywedcoder@gmail.com>
2025-08-23 18:26:36 +0530

    Create 23_inverted_alphabet_triangle.py

---
+++ Alphabet Patttern/23_inverted_alphabet_triangle/23_inverted_alphabet_triangle.py
@@ -0,0 +1,15 @@
+#Pattern 23
+#Inverted Alphabet Triangle Pattern
+
+"""
+A B C D E 
+A B C D 
+A B C
+A B
+A
+"""
+
+for i in range(69, 64, -1):
+    for j in range(65, i+1):
+        print(chr(j), end=" ")
+    print()
